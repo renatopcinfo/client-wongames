23105386371fb8fc21c1486e7609ce469cb9be16
renatopcinfo <renatopcinfo@gmail.com>
2020-10-12 14:26:57 -0300

    testing Logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -2,8 +2,22 @@ import styled, { css } from 'styled-components'
 
 import { LogoProps } from '.' 
 
+const wrapperModifiers = {
+  normal: () => css`
+    width: 11rem;
+    heigth: 3.3rem;
+  `,
+
+  large: () => css`
+    width: 20rem;
+    heigh: 5.9rem;
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color }) => css`
+  ${({ theme, color, size }) => css`
     color: ${theme.colors[color!]};
+
+    ${!!size && wrapperModifiers[size]}
   `}
 `
