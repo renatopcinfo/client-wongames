bd3564879d1a9fcec5d2cc1fb62f0b1a48e5981f
renatopcinfo <renatopcinfo@gmail.com>
2020-11-08 23:08:15 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
@@ -19,7 +22,15 @@ export function getServerSideProps() {
   // retorno dos dados
   return {
     props: {
-      heading: 'Olha eu aqui 123'
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock
     }
   }  
 }
