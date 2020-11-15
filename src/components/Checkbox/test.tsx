97503e767f96a82622cf6adfd56c688813177671
renatopcinfo <renatopcinfo@gmail.com>
2020-11-15 00:33:09 -0300

    checked test

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Checkbox from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
@@ -33,4 +34,18 @@ describe('<Checkbox />', () => {
       color: theme.colors.black
     })
   })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+  })
+
 })
