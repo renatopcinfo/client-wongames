8635086705f3a928d3749b267c42e1b7f2d786f7
renatopcinfo <renatopcinfo@gmail.com>
2020-11-25 23:15:10 -0300

    textcontent tests

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -15,5 +15,32 @@ describe('<TextContent />', () => {
     expect(
       screen.getByRole('heading', { name: /description/i })
     ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /content/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(<TextContent content={props.content} />)
+
+    expect(
+      screen.queryByRole('heading', { name: /description/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render the title and content', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', { name: /description/i })
+      .parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
   })
 })
