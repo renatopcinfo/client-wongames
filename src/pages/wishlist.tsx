f5cae26abc4c85e282e9fa3cd872f7e34eccb2ed
renatopcinfo <renatopcinfo@gmail.com>
2020-11-30 20:06:52 -0300

    showcase

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,5 +1,17 @@
-import WishList from 'templates/WishList'
+import WishList, { WishlistTemplateProps } from 'templates/WishList'
 
-export default function WishListPage() {
-  return <WishList />
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function WishListPage(props: WishlistTemplateProps) {
+  return <WishList {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighLight: highlightMock
+    }
+  }
 }
--- src/templates/WishList/index.tsx
+++ src/templates/WishList/index.tsx
@@ -1,14 +1,32 @@
 import Base from 'templates/Base'
-import { Container } from 'components/Container'
+
 import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
 
 import * as S from './styles'
 
-const WishList = () => (
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighLight: HighlightProps
+}
+
+const WishList = ({
+   recommendedGames,
+   recommendedHighLight 
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">Wishlist</Heading>
     </Container>
+
+    <Showcase 
+      title="You may like these games" 
+      games={recommendedGames} 
+      highlight={recommendedHighLight}
+   />
   </Base>
 )
 
