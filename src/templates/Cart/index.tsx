b6c834707c81abc419d5457085d56d953f4ad22c
renatopcinfo <renatopcinfo@gmail.com>
2020-12-10 21:51:11 -0300

    styles

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,6 +1,7 @@
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
@@ -15,36 +16,46 @@ export type CartProps = {
 } & CartListProps &
   Pick<PaymentOptionsProps, 'cards'>
 
-const Cart = ({ 
-  recommendedGames, 
-  recommendedHighlight, 
-  items, 
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
   total,
-  cards,
- }: CartProps) => {
+  cards
+}: CartProps) => {
   const handlePayment = () => ({})
 
-   return (     
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        My cart
-      </Heading>
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My cart
+        </Heading>
 
-      <S.Content>
-        <CartList items={items} total={total} />
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
 
-        <PaymentOptions cards={cards} handlePayment={handlePayment} />
-      </S.Content>
-      <Divider />
-    </Container>
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <Empty
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
 
-    <Showcase 
-      title="You may like these games" 
-      games={recommendedGames} 
-      highlight={recommendedHighlight}
-   />
-  </Base>
-)}
+        <Divider />
+      </Container>
+
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
 
 export default Cart
