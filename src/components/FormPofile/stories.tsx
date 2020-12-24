46ad2151630ba0536d5a07c8d437fcc351810084
renatopcinfo <renatopcinfo@gmail.com>
2020-12-24 20:34:27 -0300

    formprofile

---
+++ src/components/FormPofile/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import FormPofile from '.'
+
+export default {
+  title: 'FormPofile',
+  component: FormPofile
+} as Meta
+
+export const Default: Story = () => <FormPofile />
