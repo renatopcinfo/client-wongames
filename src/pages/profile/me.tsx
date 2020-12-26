ee931b9c7ac1a1516b7e6145ea15c886c4ee3d75
renatopcinfo <renatopcinfo@gmail.com>
2020-12-26 19:04:20 -0300

    page profile

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,10 @@
+import FormPofile from 'components/FormPofile'
+import Profile from 'templates/Profile'
+
+export default function Me() {
+  return (
+    <Profile>
+      <FormPofile />
+    </Profile>
+  )
+}
